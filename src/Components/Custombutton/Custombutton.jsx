2496538c76c2bebb22d620f48352374d42602af2
Dave86dev <davidochando1986@gmail.com>
2022-11-03 13:44:10 +0100

    Operating component added to our project, after refactor

--- src/Components/Custombutton/Custombutton.jsx
+++ src/Components/Custombutton/Custombutton.jsx
@@ -1,12 +1,54 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import "./Custombutton.css";
 
 const Custombutton = () => {
 
-    return(
-        <div className="customButtonDesign">Click me!</div>
+    //When mounting, first looks for the Hooks, always.
+
+    const [sum, setSum] = useState(0);
+
+
+    useEffect(() => {
+        //This function will trigger once the component has been mounted ONLY for the first time.
+
+        console.log("The component has been mounted, and sum value is: ", sum);
+    }, []);
+
+
+    useEffect(() => {
+        //This will trigger, EVERYTIME the component updates.
+        if (sum !== 0) {
+
+            console.log("now the component is being rendered again and value is: ", sum);
+        }
+    });
+
+    //Normal functions
+
+    const operate = (operation) => {
+
+        if(operation === "+"){
+
+            setSum(sum + 5);
+        }else{
+            setSum(sum - 5);
+        };
+
+    };
+
+
+    //Second, he goes to the render zone and displays it...
+
+    return (
+        <div className="customButtonDesign">
+            <div className="sumDesign" onClick={() => operate("+")}>+</div>
+            {sum}
+            <div className="restDesign" onClick={() => operate("-")}>-</div>
+            
+        </div>
+            
     )
 }
 
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,13 +1,25 @@
 
-import React from 'react';
+//In order to be able to work with Hooks, I will import useState.
+import React, {useState, useEffect} from 'react';
+import Custombutton from '../../Components/Custombutton/Custombutton';
+
+//useEffect are the state hooks
 
 import "./Home.css";
 
 const Home = () => {
 
+    
+
+
+    
 
     return (
-        <div className="homeDesign">i am Home</div>
+        <div className="homeDesign">
+            <Custombutton/>
+            <Custombutton/>
+            
+        </div>
     )
 }
 
