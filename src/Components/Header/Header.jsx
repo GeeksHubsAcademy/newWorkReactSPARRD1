8371630a4526a9bc5b02642c5d44824914d64f9f
Dave86dev <davidochando1986@gmail.com>
2022-11-03 11:58:04 +0100

    useNavigate added to our app, working properly

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,23 @@
 
 import React from 'react';
 
+import {useNavigate} from 'react-router-dom';
+
 import "./Header.css";
 
 const Header = () => {
 
+    //I am going to execute useNavigate in order to create an instance...and store it inside a variable
+
+    let navigate = useNavigate();
+
     return(
         <div className="headerDesign">
-            Login Register Home Profile
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/login")},750)}>Login</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/register")},750)}>Register</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/")},750)}>Home</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/profile")},750)}>Profile</div>
+            
         </div>
     )
 
