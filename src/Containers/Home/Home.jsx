d0d0a078ead27d5f3405668fd1313862042eb168
Dave86dev <davidochando1986@gmail.com>
2022-11-03 14:29:53 +0100

    Mapping data from an API

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,6 +1,7 @@
 
 //In order to be able to work with Hooks, I will import useState.
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Custombutton from '../../Components/Custombutton/Custombutton';
 
 //useEffect are the state hooks
@@ -9,18 +10,72 @@ import "./Home.css";
 
 const Home = () => {
 
-    
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+
+        //This useEffect is being executed ONLY when the component is mounted for the first time.
+
+    }, []);
+
+    useEffect(() => {
+
+        setTimeout(()=>{
+
+            bringCharacters();
+        }, 3000);
+
+        console.log("now the value is...:", characters);
+    });
 
 
-    
+    const bringCharacters = async () => {
 
-    return (
-        <div className="homeDesign">
-            <Custombutton/>
-            <Custombutton/>
-            
-        </div>
-    )
+        try {
+
+            let res = await axios.get("https://rickandmortyapi.com/api/character");
+
+            if (!characters[1]) {
+                //This comparisson would mean that there is NOTHING inside the characters array
+                setCharacters(res.data.results);
+            };
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+    };
+
+    const nameMe = (person) => {
+        console.log(person.name);
+    }
+
+    if (characters[1]) {
+
+        return (
+            <div className="homeDesign">
+                {
+                    characters.map(person => {
+                        return (
+                            <div key={person.id} onClick={()=>nameMe(person)}>{person.name}</div>
+                        )
+                    })
+                }
+
+            </div>
+        )
+
+    } else {
+        return (
+            <div className="homeDesign">
+                <Custombutton />
+                <Custombutton />
+                ...THE CHARACTERS ARE LOADING...
+            </div>
+        )
+    }
+
 }
 
 export default Home;
